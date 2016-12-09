a4cb1a932b56a3bb94b2e232a851e0542364f922
Vladislav Prosvirnin <prosvirnin.vlad@gmail.com>
2016-12-09 15:02:08 +0300

    Separate RSA and AES classes.

--- messenger/secure.py
+++ messenger/secure.py
@@ -2,35 +2,50 @@ from Crypto import Random
 from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
 
-class Secure:
-	AES_KEY_SIZE = 16
-	RSA_KEY_SIZE = 2048
+class Rsa:
+	KEY_SIZE = 2048
 
 	def __init__(self):
-		self.gen_keys()
+		self.gen_key()
 
-	def gen_keys(self):
-		self._aes_key = Random.new().read(Secure.AES_KEY_SIZE)
-		self._aes_handler = AES.new(self._aes_key)
-		self._rsa_prv_key = RSA.generate(Secure.RSA_KEY_SIZE)
-		self._rsa_pub_key = self._rsa_prv_key.publickey()
+	def gen_key(self):
+		self._prv_key = RSA.generate(Rsa.KEY_SIZE)
+		self._pub_key = self._prv_key.publickey()
 
-	def encode_aes_key(self):
-		aes_key, *_ = self._rsa_pub_key.encrypt(self._aes_key, None)
-		return aes_key
+	def encode(self, buf):
+		buf, *_ = self._pub_key.encrypt(buf, None)
+		return buf
 
-	def import_pub_rsa_key(self, pub_key):
-		self._rsa_pub_key = RSA.importKey(key)
+	def decode(self, buf):
+		return self._prv_key.decrypt(buf)
 
-	def export_pub_rsa_key(self):
-		return self._rsa_pub_key.exportKey("PEM")
+	def import_pub_key(self, pub_key):
+		self._pub_key = RSA.importKey(pub_key)
 
-	def encode_message(self, message):
-		message_len = 16 * (len(message) // 16 + 1)
-		message += "\x00" * (message_len - len(message))
-		return self._aes_handler.encrypt(message.encode())
+	def export_pub_key(self):
+		return self._pub_key.exportKey("PEM")
 
-	def decode_message(self, message):
-		message = self._aes_handler.decrypt(message).decode()
-		return message.rstrip("\x00")
+class Aes:
+	KEY_SIZE = 16
 
+	def __init__(self):
+		self.gen_key()
+
+	def gen_key(self):
+		self._key = Random.new().read(Aes.KEY_SIZE)
+		self._handler = AES.new(self._key)
+
+	def encode(self, buf):
+		buf_len = 16 * (len(buf) // 16 + 1)
+		buf += b"\x00" * (buf_len - len(buf))
+		return self._handler.encrypt(buf)
+
+	def decode(self, buf):
+		return self._handler.decrypt(buf)
+
+	def import_key(self, key):
+		self._key = key
+		self._handler = AES.new(key)
+
+	def export_key(self):
+		return self._key
